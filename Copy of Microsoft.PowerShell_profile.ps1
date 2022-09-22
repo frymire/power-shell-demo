c4a2873b0ff9081181d5e402d073e0495a5a8fe6
Mark Frymire <mark.frymire@icloud.com>
2022-09-22 17:32:55 -0400

    Add copy of PowerShell profile

---
+++ Copy of Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,10 @@
+# Put this in C:\Users\mark.frymire.SERCO-NA\Documents\PowerShell\
+
+cd "D:\code"
+
+# Show navigable menu of all options when hitting Tab
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+
+# Enable autocompletion for arrow keys.
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
